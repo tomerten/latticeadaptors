267e7e8979b37c5b291f337a44fc3981628a5a7b
Tom Mertens <tom.mertens@helmholtz-berlin.de>
2021-05-01 13:45:53 +0200

    added directory structure added madx_seq_parser from string and simple first test

--- tests/test_latticeadaptors.py
+++ tests/test_latticeadaptors.py
@@ -1,5 +1,29 @@
-from latticeadaptors import __version__
+import pandas as pd
+import pytest
+from latticeadaptors import __version__, parse_from_madx_sequence_string
+from pandas.testing import assert_frame_equal
+
+# test inputs and outputs
+seq_str_test = [
+    (
+        """
+    testmarker : MARKER;
+    """,
+        None,
+        0.0,
+        pd.DataFrame([{"family": "MARKER", "name": "TESTMARKER"}]),
+    ),
+]
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+@pytest.mark.parametrize("string, expected_name, expected_len, expected_df", seq_str_test)
+def test_parse_from_madx_seqeunce_string(string, expected_name, expected_len, expected_df):
+    name, length, df = parse_from_madx_sequence_string(string)
+
+    assert name == expected_name
+    assert length == expected_len
+    assert_frame_equal(df, expected_df, check_like=True)
