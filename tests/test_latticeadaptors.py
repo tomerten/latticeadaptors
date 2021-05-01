c04ae50c04e52a1bbefefd04542fa0fdb6b60826
Tom Mertens <tom.mertens@helmholtz-berlin.de>
2021-05-01 15:45:50 +0200

    added test for sequence

--- tests/test_latticeadaptors.py
+++ tests/test_latticeadaptors.py
@@ -13,6 +13,16 @@ seq_str_test = [
         0.0,
         pd.DataFrame([{"family": "MARKER", "name": "TESTMARKER"}]),
     ),
+    (
+        """
+    FODO: SEQUENCE, L=8;
+    QF  , at = 2.000;
+    ENDSEQUENCE;
+    """,
+        "FODO",
+        8.0,
+        pd.DataFrame([{"name": "QF", "pos": 2.0}]),
+    ),
 ]
 
 
@@ -24,6 +34,7 @@ def test_version():
 def test_parse_from_madx_seqeunce_string(string, expected_name, expected_len, expected_df):
     name, length, df = parse_from_madx_sequence_string(string)
 
+    print(df)
     assert name == expected_name
     assert length == expected_len
     assert_frame_equal(df, expected_df, check_like=True)
