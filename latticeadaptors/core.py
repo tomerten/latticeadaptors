38c93e9b10ac8ea322849efaf87ce6018f53856b
Tom Mertens <tom.mertens@helmholtz-berlin.de>
2021-05-02 11:53:26 +0200

    updated main class to have elegant parsers and madx install start and end marker

--- latticeadaptors/core.py
+++ latticeadaptors/core.py
@@ -4,9 +4,15 @@ from latticeadaptors import (
     __version__,
     parse_from_madx_sequence_file,
     parse_from_madx_sequence_string,
+    parse_table_to_elegant_file,
+    parse_table_to_elegant_string,
+    parse_table_to_madx_install_str,
+    parse_table_to_madx_remove_str,
     parse_table_to_madx_sequence_file,
     parse_table_to_madx_sequence_string,
 )
+
+from .Utils.MadxUtils import install_start_end_marke
 
 
 class LatticeAdaptor:
@@ -40,5 +46,17 @@ class LatticeAdaptor:
         return parse_table_to_madx_sequence_string(self.name, self.len, self.table)
 
     def parse_table_to_madx_sequence_file(self, filename):
-        """Parse table to madx sequence and return it as a string"""
-        return parse_table_to_madx_sequence_file(self.name, self.len, self.table, filename)
+        """Parse table to madx sequence and write to file"""
+        parse_table_to_madx_sequence_file(self.name, self.len, self.table, filename)
+
+    def parse_table_to_elegant_string(self):
+        """Parse table to elegant lattice file and return as string."""
+        return parse_table_to_elegant_string(self.name, self.table)
+
+    def parse_table_to_elegant_file(self, filename):
+        """Parse table to elegant lattice and write to file"""
+        parse_table_to_elegant_file(self.name, self.table, filename)
+
+    def madx_sequence_add_start_end_marker_string(self):
+        """Return madx string to install marker at start and at end of lattice"""
+        return install_start_end_marker(self.name, self.len)
